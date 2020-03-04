aa724ac6110ae46dc11cf9efb3dcef86aa0c66cc
aliciamaurtua <afmaurtua@miners.utep.edu>
2020-03-04 14:49:14 -0700

    Ran a test case then try to see where the problem is

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,8 +1,29 @@
 public class SortOfSort {
     public void SortOfSort(int[] sort){
-
-        for(int i = 0; i< sort.length; i++){
+        int count = 0;
+        int max = 0;// index
+        for(int j = 0; j< sort.length; j++){ //looking for the largest number
+            for(int i = 1; i< sort.length-j; i++){ //sorting throught the array
+                if (sort[i] > sort[max]){ // keeping track of where it's already been sorted
+                    max = i;
+                }
+            }
+            if(count < 2){ //Actually sorting out the numbers
+                int temp = sort[max];
+                sort[max] = sort[(sort.length -1)-j];
+                sort[(sort.length-1)-j] = temp;
+                count ++;
+            }
+            else{
+                int temp = sort[max];
+                sort[max] = sort[j-2];
+                sort[j-2] = temp;
+                count ++;
+                if(count == 4){
+                    count = 0;
+                }
+            }
+            max = 1;
         }
-
     }
 }
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -1,3 +1,43 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SortOfSortTester {
+    @Test
+    public void TestSort1(){
+        int A [] = {8,7,6,4,3};
+        int B []= {6,4,3,7,8};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(A);
+
+        for(int i =0; i< A.length; i++){
+            System.out.print(A[i]);
+        }
+        for(int i =0; i< A.length; i++){
+            assertEquals(B[i], A[i]);
+        }
+
+
+    }
+    @Test
+    public void TestSort2(){
+        int C [] = {10,22,56,34,11,2};
+        int D [] = {22,10,2,11,34,56};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(C);
+
+        for(int i =0; i< C.length; i++){
+            System.out.print(C[i]);
+        }
+        for(int i =0; i< C.length; i++){
+            assertEquals(D[i], C[i]);
+        }
+
+    }
 
 }
