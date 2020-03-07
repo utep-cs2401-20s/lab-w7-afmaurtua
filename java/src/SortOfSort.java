d396d1cc49554024d5f922815e4aa68f69cd7297
aliciamaurtua <afmaurtua@miners.utep.edu>
2020-03-06 17:09:26 -0700

    Had a bug in my first algorithm, re-did my algorithm.

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,29 +1,43 @@
 public class SortOfSort {
     public void SortOfSort(int[] sort){
+        int begin = 0;
+        int end = sort.length-1;
         int count = 0;
-        int max = 0;// index
-        for(int j = 0; j< sort.length; j++){ //looking for the largest number
-            for(int i = 1; i< sort.length-j; i++){ //sorting throught the array
-                if (sort[i] > sort[max]){ // keeping track of where it's already been sorted
+        boolean rightSide = true;
+
+        while(begin != end){
+            int max = begin;
+            count++;
+
+            for(int i = begin + 1; i <= end; i++){
+                if(sort[i] > sort[max]){
                     max = i;
                 }
             }
-            if(count < 2){ //Actually sorting out the numbers
-                int temp = sort[max];
-                sort[max] = sort[(sort.length -1)-j];
-                sort[(sort.length-1)-j] = temp;
-                count ++;
+
+            if(count % 3 == 0){
+                rightSide = !rightSide;
+                count = 1;
+
             }
-            else{
+            System.out.print(begin + " " + end);
+            System.out.println(" " + max);
+            if(rightSide){
                 int temp = sort[max];
-                sort[max] = sort[j-2];
-                sort[j-2] = temp;
-                count ++;
-                if(count == 4){
-                    count = 0;
-                }
+                sort[max] = sort[end];
+                sort[end] = temp;
+                end --;
+            } else{
+                int temp = sort[max];
+                sort[max] = sort[begin];
+                sort[begin] = temp;
+                begin ++;
             }
-            max = 1;
+
+            for(int i = 0; i < sort.length;i++){
+                System.out.print(sort[i] + " ");
+            }
+            System.out.println();
         }
     }
 }
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -6,37 +6,76 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SortOfSortTester {
+   /*This was just a genral test to ensure there
+   wasn't any wrong in the method and everything ws working fine
+    */
     @Test
     public void TestSort1(){
-        int A [] = {8,7,6,4,3};
-        int B []= {6,4,3,7,8};
-
-        SortOfSort x = new SortOfSort();
-        x.SortOfSort(A);
-
-        for(int i =0; i< A.length; i++){
-            System.out.print(A[i]);
-        }
-        for(int i =0; i< A.length; i++){
-            assertEquals(B[i], A[i]);
-        }
-
-
-    }
-    @Test
-    public void TestSort2(){
         int C [] = {10,22,56,34,11,2};
-        int D [] = {22,10,2,11,34,56};
+        int D [] = {22,11,2,10,34,56};
 
         SortOfSort x = new SortOfSort();
         x.SortOfSort(C);
+        assertArrayEquals(D, C);
+    }
+    /*The method is suppose to handle positive and
+    negative numbers and this test case shows that it can
+    handle a negavite number, and recognize that its
+    the smallest number
+     */
 
-        for(int i =0; i< C.length; i++){
-            System.out.print(C[i]);
-        }
-        for(int i =0; i< C.length; i++){
-            assertEquals(D[i], C[i]);
-        }
+    @Test
+    public void TestSort2(){
+        int A [] = {8,7,6,-4,3};
+        int B []= {6,3,-4,7,8};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+    /*For this test case the expected output
+     was to make sure that the method could handle and organize
+     all negative values.
+     */
+    @Test
+    public void TestSort3(){
+        int A [] = {-10,-3,-5,-32,-1};
+        int B [] = {-5,-10,-32,-3,-1};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+    /*Reason for this test case is because was wanting to test
+    big values and the output and also to test that is puts a duplicate
+    number in the right place and not side by side.
+     */
+
+    @Test
+    public void TestSort4(){
+        int A [] = {55,45,78,68,55,99};
+        int B [] = {68,55,45,55,78,99};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(A);
+        assertArrayEquals(B, A);
+
+    }
+    /*Choose this particular test case becasue
+    the input is a small three number array and tested that it could handle
+    the small array of three and put them in the right "order."
+     */
+
+    @Test
+    public void TestSort5(){
+        int A [] = {7,11,5};
+        int B [] = {5,7,11};
+
+        SortOfSort x = new SortOfSort();
+        x.SortOfSort(A);
+        assertArrayEquals(B, A);
 
     }
 
